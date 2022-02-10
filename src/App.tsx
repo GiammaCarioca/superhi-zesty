98739bb81d6ebc1a8f2bccd18f039ef5000e5d39
giammacarioca <giammattey@gmail.com>
2022-02-10 13:30:52 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import { FC, useState } from 'react'
+import './styles.css'
+import cabbage from 'assets/image1.jpeg'
+import mango from 'assets/image2.jpeg'
+import fig from 'assets/image3.jpeg'
+import gaze from 'assets/image4.jpeg'
+import peach from 'assets/image5.jpeg'
+import avocado from 'assets/image6.jpeg'
+
+const images: string[] = [cabbage, mango, fig, gaze, peach, avocado]
+
+interface ILoadingProps {
+  calculatedWidth: number
+}
+
+const Loading: FC<ILoadingProps> = ({ calculatedWidth }): JSX.Element => (
+  <aside>
+    <div className="loading-bar">
+      <label htmlFor="images-loaded">Loading all your favorite images...</label>
+      <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
+    </div>
+  </aside>
+)
+
+const App: FC = (): JSX.Element => {
+  const [currentImage, setCurrentImage] = useState<number>(0)
+  const [numLoaded, setNumLoaded] = useState<number>(0)
+
+  const handleClick = (): void => {
+    const length = images.length - 1
+
+    setCurrentImage((currentImage) =>
+      currentImage < length ? currentImage + 1 : 0
+    )
+  }
+
+  const handleImageLoad = (): void => {
+    setNumLoaded((numLoaded) => numLoaded + 1)
+  }
+
+  return (
+    <section>
+      <header>
+        <h1>Zesty</h1>
+        <h2>
+          A photography project <br /> by Ella Fieldling
+        </h2>
+      </header>
+
+      <figure>
+        {numLoaded < images.length && (
+          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+        )}
+        <figcaption>
+          {currentImage + 1} / {images.length}
+        </figcaption>
+        {images.map((imageURL, index) => (
+          <img
+            alt=""
+            key={imageURL}
+            src={imageURL}
+            onClick={handleClick}
+            onLoad={handleImageLoad}
+            className={currentImage === index ? 'display' : 'hide'}
+          />
+        ))}
+      </figure>
+    </section>
+  )
+}
+
+export default App
